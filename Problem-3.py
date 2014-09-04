31a9b16240e621469f52b8e100927155d786bfd4
Daniel Backx <dbackx11@gmail.com>
2014-09-03 17:27:39 -0800

    Update Problem-3.py

--- Problem-3.py
+++ Problem-3.py
@@ -1,26 +1,59 @@
-Found=True
-div = 20
-num=0
-x=0
+
+#Problem 3: Largest prime factor
+#
+#Source: https://projecteuler.net/problem=3
+#
+# The prime factors of 13195 are 5, 7, 13 and 29.
+#
+# What is the largest prime factor of the number 600851475143 ?
+#
+#Daniel Backx
+#
+
+import getPrime
+from math import sqrt
+
+userInput = input('\nProblem 3: Largest prime factor\n\n\tThe prime factors of 13195 are 5, 7, 13 and 29\n\n What is the largest prime factor of the number: ')
+num = int(userInput)
 
 
-getDenom	
+#Currently not working, much faster & more efficient version
+if False:
+    for div in range(2, int(sqrt(num))):
+        if (num%div==0 and all(div%checkPrime!=0 for checkPrime in range (2, int(sqrt(div))))==True):
+            maxPDiv = div
 
-while Found:
-	num+=div*(div-1)
-	print (num)
-	z=0
-	for x in range (1, div+1):
-		print(x)
-		if (num%x!=0):
-			break
-		else:
-			z+=1
-	if (z==div):
-		Found=False
+    print('Max prime divisor is:',div)
 
-print('ANSWER: ', num)
 
-232792560
-174594420
+#Old method. takes too long, needs fix
+flag=False
+div = int(num/2)
+if (num==4): print('The largest prime factor of', num,' is: 4\n\n')
+else: 
+    if (div%2==0): div-=1
+    while (div!=1):
+        if (num%div==0 and getPrime.primeCheck(div)):
+            flag=True
+            print('The largest prime factor of', num,' is: ',div, '\n\n')
+            break
+        div-=2
+
+    if (flag==False): print('\nThere is no prime factor of:',num,'\n\n')
+
+
+
+#Older Method, terrible, took too long. Waste of time & memory
+if False:
+    flag=False
+    prime = 2
+    while (prime<num/2):
+        if ((num%prime)==0):
+            largestPrime = prime
+            flag=True
+            print(prime, 'is a factor')
+        prime = getPrime.nextPrime(prime)
+
+    if (flag): print('The largest prime factor of', num,' is: ',largestPrime, '\n\n')
+    else: print('\nThere is no prime factor of:',num,'\n\n')
 
