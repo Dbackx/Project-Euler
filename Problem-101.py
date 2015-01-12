b3cfc7ee2a978c6a5fa50361c108366b6802ba4f
Daniel Backx <dbackx11@gmail.com>
2015-01-12 17:51:27 -0500

    Tidying, and stopped Problem 101 from running as it is incomplete.

--- Problem-101.py
+++ Problem-101.py
@@ -44,10 +44,13 @@ def f(n):
 	fn = 1 - n + n**2 - n**3 + n**4 - n**5 + n**6 - n**7 + n**8 - n**9 + n**10
 	return fn
 
-x=1
+while False:
+	x=1
+	n=1
+	fsequence = f(1), f(2), f(3), f(4)
 
-n=1
-fsequence = f(1), f(2), f(3), f(4)
+	print fsequence
 
-print fsequence
+print ("Problem Incomplete, sorry, check again later")
+
 print ("Problem",__problem__,"took "+str(round(time.clock()-t,3))+"s to complete\n")
